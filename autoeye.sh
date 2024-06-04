a68cd3bd75dae360216d0777637dcca0e4fc9cfe
Tannahsheen <152140112+Tannahsheen@users.noreply.github.com>
2024-06-04 15:06:07 -0500

    Add files via upload

---
+++ autoeye.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+if ! command -v eyewitness &> /dev/null
+then
+    echo "Eyewitness could not be found, installing now..."
+    sudo apt-get install eyewitness -y
+fi
+
+read -p "Enter the path to the file containing IP ranges: " IP_FILE
+
+if [ ! -f "$IP_FILE" ]; then
+    echo "File not found: $IP_FILE"
+    exit 1
+fi
+
+TARGET_IP_RANGE=$(cat $IP_FILE)
+
+PORTS="80,443,8080,8000,8443,8081,591,82,8880,8008,8081"
+
+masscan -p $PORTS $TARGET_IP_RANGE --rate=1000 -oG - | awk '/open$/{print $2}' > live_hosts.txt
+
+echo "Live IP addresses saved to live_hosts.txt"
+
+eyewitness --web -f live_hosts.txt

804dafa3d1d23f29d4acdfedb49b3605b4a22c9c
Tannahsheen <152140112+Tannahsheen@users.noreply.github.com>
2025-03-12 16:53:04 -0500

    Update autoeye.sh
    trying to fix awk here

--- autoeye.sh
+++ autoeye.sh
@@ -37,7 +37,7 @@ if [ $? -ne 0 ]; then
 fi
 
 # Extract live IPs with ports
-awk '/Host:/ {print $4 ":" $6}' masscan_output.txt | sort -u | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$' > live_hosts.txt
+awk '/Host:/ {split($4, port, "/"); print $2 ":" port[1]}' masscan_output.txt | sort -u | grep -E '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+:[0-9]+$' > live_hosts.txt
 if [ ! -s live_hosts.txt ]; then
     echo "No live hosts found. Exiting."
     rm masscan_output.txt
